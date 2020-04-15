f15d59e5ed78d71db2735731dd7fe39ae3c0aaba
stoffee <chris@roadkill.org>
2020-04-15 12:29:58 -0700

    Update main.tf

--- identity/azure-auth/main.tf
+++ identity/azure-auth/main.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
-  version = "~> 1.x"
+  version = "=1.44.0"
 }
 
 resource "azurerm_resource_group" "tf_rg" {
     name     = "${var.resource_group_name}"
-    location = "eastus"
+    location = "westus2"
 
     tags {
         environment = "${var.resource_group_name}"
